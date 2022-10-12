3d41b3e8e6b64600bbb70844cd5d22f7425d857e
Ondřej Mach <ondrej.mach@seznam.cz>
2022-10-12 14:55:02 +0200

    Added tables to the database.

--- docker/db_server/init_db.sql
+++ docker/db_server/init_db.sql
@@ -6,7 +6,9 @@ CREATE TABLE Account (
     accountRealName VARCHAR(128),
     accountAddress VARCHAR(128),
     accountDateOfBirth DATE,
-    accountEmail VARCHAR(128)
+    accountEmail VARCHAR(128),
+    accountTeacher BOOLEAN,
+    accountStudent BOOLEAN
 );
 
 CREATE TABLE Login (
@@ -23,22 +25,63 @@ CREATE TABLE Admin (
 );
 
 CREATE TABLE Room (
-    roomID INTEGER NOT NULL,
+    roomID INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
     roomName VARCHAR(128),
     roomDescription VARCHAR(128)
 );
 
 CREATE TABLE Course (
-    courseID INTEGER NOT NULL,
+    courseID INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
     courseName VARCHAR(128),
-    courseDescription VARCHAR(128),
-    courseGuarantorID INTEGER,
-    FOREIGN KEY (courseGuarantorID) REFERENCES Account(accountID)
+    courseDescription VARCHAR(128)
+);
+
+CREATE TABLE Guarantees (
+    accountID INTEGER NOT NULL,
+    courseID INTEGER UNIQUE NOT NULL,
+    approved BOOLEAN,
+    FOREIGN KEY (accountID) REFERENCES Account(accountID),
+    FOREIGN KEY (courseID) REFERENCES Course(courseID)
+);
+
+CREATE TABLE Attends (
+    accountID INTEGER NOT NULL,
+    courseID INTEGER NOT NULL,
+    approved BOOLEAN,
+    FOREIGN KEY (accountID) REFERENCES Account(accountID)
+);
+
+CREATE TABLE Lecture (
+    courseID INTEGER NOT NULL,
+    roomID INTEGER,
+    lectureFrom DATE,
+    lectureTo DATE,
+    FOREIGN KEY (courseID) REFERENCES Course(courseID),
+    FOREIGN KEY (roomID) REFERENCES Room(roomID)
 );
 
 CREATE TABLE Lector (
     accountID INTEGER NOT NULL,
-    courseID VARCHAR(128),
-    FOREIGN KEY (accountID) REFERENCES Account(accountID)
+    courseID INTEGER NOT NULL,
+    FOREIGN KEY (accountID) REFERENCES Account(accountID),
+    FOREIGN KEY (courseID) REFERENCES Course(courseID)
 );
 
+CREATE TABLE Term (
+    courseID INTEGER NOT NULL,
+    termID INTEGER PRIMARY KEY NOT NULL,
+    termName VARCHAR(128),
+    termDate DATE,
+    termMaxPoints INTEGER,
+    termAutoregistered BOOLEAN,
+    FOREIGN KEY (courseID) REFERENCES Course(courseID)
+);
+
+CREATE TABLE Points (
+    termID INTEGER,
+    accountID INTEGER,
+    termName VARCHAR(128),
+    termDate DATE,
+    termMaxPoints INTEGER,
+    FOREIGN KEY (termID) REFERENCES Term(termID)
+);
