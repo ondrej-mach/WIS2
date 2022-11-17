6be9b342a22123fc173897b5be691351d5e54c8b
Rostislav Lán <lanrosta@seznam.cz>
2022-11-17 13:35:45 +0100

    Updated db init file.

--- docker/db_server/insert_data.sql
+++ docker/db_server/insert_data.sql
@@ -19,9 +19,13 @@ VALUES (4, 'cerna', 'Jana Černá', '$1$fizRusmT$KJ9kwwjzp8ATnmSBMq4o4.', "1999/
 INSERT INTO Account (accountID, accountUsername, accountRealName, accountPassword, accountDateOfBirth, accountEmail, accountTeacher) 
 VALUES (5, 'havel', 'Miroslav Havel', '$1$fizRusmT$KJ9kwwjzp8ATnmSBMq4o4.', "2003/6/6", "havelmira@gmail.com", true);
 
--- login xnovak00, heslo student
+-- login xnovot13, heslo student
 INSERT INTO Account (accountID, accountUsername, accountPassword, accountDateOfBirth, accountEmail, accountStudent) 
-VALUES (6, 'xnovot13', '$1$J2b.7qT3$C6m3z10By7kfob8Ikx0N10', "2001/7/12", "xnovak00@fit.vutbr.cz", true);
+VALUES (6, 'xnovot13', '$1$J2b.7qT3$C6m3z10By7kfob8Ikx0N10', "2001/7/12", "xnovot13@fit.vutbr.cz", true);
+
+-- login xfitko12, heslo student
+INSERT INTO Account (accountID, accountUsername, accountPassword, accountDateOfBirth, accountEmail, accountStudent) 
+VALUES (7, 'xfitko12', '$1$J2b.7qT3$C6m3z10By7kfob8Ikx0N10', "2003/5/8", "xfitko@fit.vutbr.cz", true);
 
 
 -- IIS course
@@ -32,13 +36,22 @@ VALUES (1, 'IIS', 'Information systems', 'This course will teach student the sec
 INSERT INTO Course (courseID, courseName, courseFullName, courseDescription, courseState, courseCredits, courseCapacity)
 VALUES (2, 'IMA1', 'Mathematical Analysis', 'math stuff, integrals, ...', 10, 5, 600);
 
--- teachers, guarantors for courses
+-- IMA1 course
+INSERT INTO Course (courseID, courseName, courseFullName, courseDescription, courseState, courseCredits, courseCapacity)
+VALUES (3, 'ISU', 'Assemblers', 'assemblers x86', 10, 7, 700);
+
+-- guarantors for courses
 INSERT INTO Guarantees (accountID, courseID) VALUES (2, 1);
 INSERT INTO Guarantees (accountID, courseID) VALUES (4, 2);
+INSERT INTO Guarantees (accountID, courseID) VALUES (5, 3);
 
+-- teachers for courses
+INSERT INTO Lecturer (accountID, courseID) VALUES (2, 1);
 INSERT INTO Lecturer (accountID, courseID) VALUES (4, 1);
+INSERT INTO Lecturer (accountID, courseID) VALUES (4, 3);
 INSERT INTO Lecturer (accountID, courseID) VALUES (5, 1);
 INSERT INTO Lecturer (accountID, courseID) VALUES (5, 2);
+INSERT INTO Lecturer (accountID, courseID) VALUES (5, 3);
 
 -- D105 room
 INSERT INTO Room (roomID, roomName, roomDescription) 
@@ -63,3 +76,12 @@ VALUES (3, 2, 'Bonusový úkol', '2022/10/1', '2');
 INSERT INTO SignedUp (studentID, termID) VALUES (3, 1);
 INSERT INTO SignedUp (studentID, termID) VALUES (6, 2);
 INSERT INTO SignedUp (studentID, termID) VALUES (3, 2);
+
+-- student attends course
+INSERT INTO Attends (studentID, courseID, approved) VALUES (3, 1, true);
+INSERT INTO Attends (studentID, courseID, approved) VALUES (3, 2, true);
+INSERT INTO Attends (studentID, courseID, approved) VALUES (6, 1, true);
+INSERT INTO Attends (studentID, courseID, approved) VALUES (6, 3, true);
+INSERT INTO Attends (studentID, courseID, approved) VALUES (7, 1, true);
+INSERT INTO Attends (studentID, courseID, approved) VALUES (7, 2, false);
+INSERT INTO Attends (studentID, courseID, approved) VALUES (7, 3, true);
