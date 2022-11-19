4f624b52e971ec7404351576b29e0f42253aeeff
Rostislav Lán <lanrosta@seznam.cz>
2022-11-19 19:18:54 +0100

    Revert "Not really a fix, but had to rename some variables in order to avoid conflict with superglobals."
    This reverts commit b1e6c2b6c05bd91c93758926bc6c23739f93a9fc.

--- www/html/usermod.php
+++ www/html/usermod.php
@@ -1,16 +1,12 @@
 <?php
-
 require_once 'includes/authorization-inc.php';
+
 assert_logged_in();
 
-
-
+$uid = $GLOBALS['user']->accountID;
 if (isset($_REQUEST["accountID"])) {
     assert_admin();
     $uid = $_REQUEST["accountID"];
-}
-else {
-    $uid = $GLOBALS['user']->accountID;
 }
 
 $userParams = [ 
@@ -57,8 +53,6 @@ if (!empty($attributes)) {
 
 <section class="section_form">
     <?php
-        require_once 'includes/users-inc.php';
-
         if (is_admin()) {
             echo "<a class=\"button_back\" href=manageusers.php>Back to user management</a><br/>";
         }
@@ -66,42 +60,39 @@ if (!empty($attributes)) {
         echo "<div><form method=\"POST\">";
         $disabled = is_admin() ? '' : 'disabled';
         
-        // !!! for some reason, this cannot be called $user !!!
-        $user_to_process = getUserByID($uid);
+        require_once 'includes/users-inc.php';
+        $user = getUserByID($uid);
         
         echo "<label>Username<input name=\"accountUsername\" type=\"text\" 
-        $disabled value=\"$user_to_process->accountUsername\"></label><br/>";
+        $disabled value=\"$user->accountUsername\"></label><br/>";
         
         echo '<label>Name<input name="accountRealName" type="text" value="'.
-        $user_to_process->accountRealName.'"></label><br/>';
+        $user->accountRealName.'"></label><br/>';
         
         echo '<label>Address<input name="accountAddress" type="text" value="'.
-        $user_to_process->accountAddress.'"></label><br/>';
+        $user->accountAddress.'"></label><br/>';
 
         echo '<label>Date of birth<input name="accountDateOfBirth" type="date" value="'.
-        $user_to_process->accountDateOfBirth.'"></label><br/>';
+        $user->accountDateOfBirth.'"></label><br/>';
     
         echo '<label>Email<input name="accountEmail" type="text" value="'.
-        $user_to_process->accountEmail.'"></label><br/>';
+        $user->accountEmail.'"></label><br/>';
         
         #TODO uncheck user not working properly
-        $checked_a = $user_to_process->accountAdmin ? "checked" : "";
-        $checked_t = $user_to_process->accountTeacher ? "checked" : "";
-        $checked_s = $user_to_process->accountStudent ? "checked" : "";
+        $checked_a = $user->accountAdmin ? 'checked' : '';
+        $checked_t = $user->accountTeacher ? 'checked' : '';
+        $checked_s = $user->accountStudent ? 'checked' : '';
         if (is_admin()) {
             echo '  <label>Admin
-                        <input type="hidden" name="accountAdmin" value="off">
-                        <input type="checkbox" name="accountAdmin" '.$checked_a.'>
+                        <input type="checkbox" name="accountAdmin"'.$checked_a.'>
                     </label><br/>';
 
             echo '  <label>Teacher
-                        <input type="hidden" name="accountTeacher" value="off">
-                        <input type="checkbox" name="accountTeacher" '.$checked_t.'>
+                        <input type="checkbox" name="accountTeacher"'.$checked_t.'>
                     </label><br/>';
 
             echo '  <label>Student
-                        <input type="hidden" name="accountStudent" value="off">
-                        <input type="checkbox" name="accountStudent" '.$checked_s.'>
+                        <input type="checkbox" name="accountStudent"'.$checked_s.'>
                     </label><br/>';
         }
     ?>
