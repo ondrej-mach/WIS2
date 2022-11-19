573577cbee81e2e41fb88f064c380028e972bdb0
Rostislav Lán <lanrosta@seznam.cz>
2022-11-19 19:10:07 +0100

    Fixed typeo in db init file.

--- docker/db_server/insert_data.sql
+++ docker/db_server/insert_data.sql
@@ -4,28 +4,28 @@ INSERT INTO Account (accountID, accountUsername, accountPassword, accountDateOfB
 VALUES (1, 'admin', '$1$1.EjW9LX$AAMfewO1RVCKyDyHAiQQ30', "2020/1/1", "admin@fit.vutbr.cz", true);
 
 -- login dvorak, heslo teacher
-INSERT INTO Account (accountID, accountUsername, accountRealName, accountPassword, accountDateOfBirth, accountEmail, accountTeacher) 
-VALUES (2, 'dvorak', 'Jaroslav Dvořák', '$1$fizRusmT$KJ9kwwjzp8ATnmSBMq4o4.', "1979/4/6", "dvorak@fit.vutbr.cz", true);
+INSERT INTO Account (accountID, accountUsername, accountRealName, accountAddress, accountPassword, accountDateOfBirth, accountEmail, accountTeacher) 
+VALUES (2, 'dvorak', 'Jaroslav Dvořák', 'Masarykovo nam., Brno','$1$fizRusmT$KJ9kwwjzp8ATnmSBMq4o4.', "1979/4/6", "dvorak@fit.vutbr.cz", true);
 
 -- login xnovak00, heslo student
-INSERT INTO Account (accountID, accountUsername, accountPassword, accountDateOfBirth, accountEmail, accountStudent) 
-VALUES (3, 'xnovak00', '$1$J2b.7qT3$C6m3z10By7kfob8Ikx0N10', "1985/8/12", "xnovak00@fit.vutbr.cz", true);
+INSERT INTO Account (accountID, accountUsername, accountRealName, accountAddress, accountPassword, accountDateOfBirth, accountEmail, accountStudent) 
+VALUES (3, 'xnovak00', 'Jan Novak', 'Vaclavske nam., Praha 1', '$1$J2b.7qT3$C6m3z10By7kfob8Ikx0N10', "1985/8/12", "xnovak00@fit.vutbr.cz", true);
 
 -- login cerna, heslo teacher
 INSERT INTO Account (accountID, accountUsername, accountRealName, accountPassword, accountDateOfBirth, accountEmail, accountTeacher) 
 VALUES (4, 'cerna', 'Jana Černá', '$1$fizRusmT$KJ9kwwjzp8ATnmSBMq4o4.', "1999/4/4", "cernaj@seznam.cz", true);
 
 -- login havel, heslo teacher
-INSERT INTO Account (accountID, accountUsername, accountRealName, accountPassword, accountDateOfBirth, accountEmail, accountTeacher) 
-VALUES (5, 'havel', 'Miroslav Havel', '$1$fizRusmT$KJ9kwwjzp8ATnmSBMq4o4.', "2003/6/6", "havelmira@gmail.com", true);
+INSERT INTO Account (accountID, accountUsername, accountRealName, accountAddress, accountPassword, accountDateOfBirth, accountEmail, accountTeacher) 
+VALUES (5, 'havel', 'Miroslav Havel', 'Vrsovicka 3, Ostrava', '$1$fizRusmT$KJ9kwwjzp8ATnmSBMq4o4.', "2003/6/6", "havelmira@gmail.com", true);
 
 -- login xnovot13, heslo student
-INSERT INTO Account (accountID, accountUsername, accountPassword, accountDateOfBirth, accountEmail, accountStudent) 
-VALUES (6, 'xnovot13', '$1$J2b.7qT3$C6m3z10By7kfob8Ikx0N10', "2001/7/12", "xnovot13@fit.vutbr.cz", true);
+INSERT INTO Account (accountID, accountUsername, accountRealName, accountAddress, accountPassword, accountDateOfBirth, accountEmail, accountStudent) 
+VALUES (6, 'xnovot13', 'Jarda Novotny', 'Bozetechova 2, Brno', '$1$J2b.7qT3$C6m3z10By7kfob8Ikx0N10', "2001/7/12", "xnovot13@fit.vutbr.cz", true);
 
 -- login xfitko12, heslo student
-INSERT INTO Account (accountID, accountUsername, accountPassword, accountDateOfBirth, accountEmail, accountStudent) 
-VALUES (7, 'xfitko12', '$1$J2b.7qT3$C6m3z10By7kfob8Ikx0N10', "2003/5/8", "xfitko@fit.vutbr.cz", true);
+INSERT INTO Account (accountID, accountUsername, accountRealName, accountAddress, accountPassword, accountDateOfBirth, accountEmail, accountStudent) 
+VALUES (7, 'xfitko12', 'Honza Fitak', 'Manesova 12, Brno', '$1$J2b.7qT3$C6m3z10By7kfob8Ikx0N10', "2003/5/8", "xfitko@fit.vutbr.cz", true);
 
 
 -- IIS course
@@ -61,27 +61,41 @@ VALUES (1, 'D105', 'Big lecture room');
 INSERT INTO Room (roomID, roomName, roomDescription)
 VALUES (2, 'E112', 'Small lecture room');
 
+-- A113 room
+INSERT INTO Room (roomID, roomName, roomDescription)
+VALUES (3, 'A113', 'Small lab');
+
 INSERT INTO Lecture (courseID, roomID) VALUES (1, 2);
 INSERT INTO Lecture (courseID, roomID) VALUES (2, 1);
 
 -- terms for the 2 courses
 INSERT INTO Term (termID, courseID, termName, termDate, termMaxPoints, termAutoregistered) 
 VALUES (1, 2, 'Půlsemestrální zkouška', '2022/11/02', '15', true);
+INSERT INTO Term (termID, courseID, termName, termDate, termMaxPoints, termAutoregistered)
+VALUES (2, 1, 'Zápočet', '2022/12/17', '30', true);
 INSERT INTO Term (termID, courseID, termName, termDate, termMaxPoints, termAutoregistered) 
-VALUES (2, 1, 'Zápočet', '2022/12/17', '0', true);
-INSERT INTO Term (termID, courseID, termName, termDate, termMaxPoints) 
-VALUES (3, 2, 'Bonusový úkol', '2022/10/1', '2');
+VALUES (3, 2, 'Bonusový úkol', '2022/10/1', '2', false);
+INSERT INTO Term (termID, courseID, termName, termDate, termMaxPoints, termAutoregistered) 
+VALUES (4, 3, 'Bonusový úkol', '2022/10/9', '4', false);
+INSERT INTO Term (termID, courseID, termName, termDate, termMaxPoints, termAutoregistered) 
+VALUES (5, 1, 'Zápočet', '2022/12/17', '15', true);
+INSERT INTO Term (termID, courseID, termName, termDate, termMaxPoints, termAutoregistered) 
+VALUES (6, 2, 'Půlsemestrální zkouška', '2022/9/02', '20', false);
+INSERT INTO Term (termID, courseID, termName, termDate, termMaxPoints, termAutoregistered) 
+VALUES (7, 3, 'Zápočet', '2022/11/19', '10', true);
 
 -- student signed up for term
 INSERT INTO SignedUp (studentID, termID, LecturerID, points) VALUES (3, 1, 5, 10);
 INSERT INTO SignedUp (studentID, termID, LecturerID, points) VALUES (6, 2, 5, 5);
-INSERT INTO SignedUp (studentID, termID, LecturerID, points) VALUES (3, 2, 4, 5);
+INSERT INTO SignedUp (studentID, termID, LecturerID, points) VALUES (3, 3, 4, 5);
 
 -- student attends course
 INSERT INTO Attends (accountID, courseID, approved) VALUES (3, 1, true);
 INSERT INTO Attends (accountID, courseID, approved) VALUES (3, 2, true);
+INSERT INTO Attends (accountID, courseID, approved) VALUES (3, 3, false);
 INSERT INTO Attends (accountID, courseID, approved) VALUES (6, 1, true);
 INSERT INTO Attends (accountID, courseID, approved) VALUES (6, 3, true);
+INSERT INTO Attends (accountID, courseID, approved) VALUES (6, 2, false);
 INSERT INTO Attends (accountID, courseID, approved) VALUES (7, 1, true);
 INSERT INTO Attends (accountID, courseID, approved) VALUES (7, 2, false);
 INSERT INTO Attends (accountID, courseID, approved) VALUES (7, 3, true);
